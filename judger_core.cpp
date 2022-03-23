ea6c1b75a5260287733ec8807f9779d31030be18
kjctar <2412864341@qq.com>
2022-03-23 16:21:04 +0800

    初步完成tracer和tracee待测试

--- judger_core.cpp
+++ judger_core.cpp
@@ -2,14 +2,14 @@
 // Created by kjctar on 2022/3/21.
 //
 
-#include <stdlib.h>
+#include <cstdlib>
 
 #include <unistd.h>
 
 #include <sys/wait.h>
 
 #include<string>
-#include<assert.h>
+#include<cassert>
 #include "language.h"
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -17,20 +17,74 @@
 #include <thread>
 #include <atomic>
 #include <sys/resource.h>
+#include <sys/ptrace.h>
+#include <stdio.h>
+#include <sys/ptrace.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/reg.h>
 
+
+
+#include <syslog.h>
+#include <cerrno>
+#include <fcntl.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <ctime>
+#include <ctype.h>
+#include <sys/wait.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/user.h>
+#include <sys/syscall.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/signal.h>
+//#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <cstring>
+#ifdef OJ_USE_MYSQL
+#include <mysql/mysql.h>
+#endif
+#include <assert.h>
 class Sandbox{
 private:
     int user;
 
 public:
+    int openChroot;
     std::string workdir;
     //初始化沙盒，编译运行环境的构建
     Sandbox(std::string  workdir){
+        openChroot=0;
+    }
 
+    void init(){
+        system(("rm -rf  "+workdir).c_str());
+        system(("mkdir -p  "+workdir+"/{lib/x86_64-linux-gnu/,lib64,bin}").c_str());
+        system(("cp /lib/x86_64-linux-gnu/libtinfo.so.6 "+workdir+"/lib/x86_64-linux-gnu/").c_str());
+        system(("cp /lib/x86_64-linux-gnu/libdl.so.2 "+workdir+"/lib/x86_64-linux-gnu/").c_str());
+        system(("cp /lib/x86_64-linux-gnu/libc.so.6 "+workdir+"/lib/x86_64-linux-gnu/").c_str());
+        system(("cp /lib64/ld-linux-x86-64.so.2 "+workdir+"/lib64").c_str());
+        system(("cp /bin/bash "+workdir+"/bin/").c_str());
     }
     //进入沙盒
-    void into(){
+    int into(){
+        int err=0;
+        err+=chdir(workdir.c_str());
+        system(("/bin/touch "+workdir+"/user.out").c_str());
+        stdout=freopen("user.out", "w", stdout);
+        stderr=freopen("error.out", "a+", stderr);
+        if(openChroot){
+            err+=chroot(workdir.c_str());
+        }
 
+
+
+        return err;
     }
 };
 
@@ -73,15 +127,16 @@ public:
     }
 
 };
-class tracer{
+class Judger{
 public:
     Report report;
     language lang;
     const int STD_MB=1024;
+    const int BUFFER_SIZE=4096;
     Sandbox box;
-    tracer(std::string SERVER_IP,int SERVER_PORT,language lang,std::string workdir):report(SERVER_IP, SERVER_PORT),lang(lang),
-                                                                                    box(workdir){
-
+    Judger(std::string SERVER_IP,int SERVER_PORT,language lang,std::string workdir):report(SERVER_IP, SERVER_PORT),lang(lang),
+                                                                                        box(workdir){
+        box.init();
     }
     //编译
     bool compile(){
@@ -90,7 +145,6 @@ public:
         {
             struct rlimit LIM;
             int cpu = 50;
-
             LIM.rlim_max = cpu;//s
             LIM.rlim_cur = cpu;
             setrlimit(RLIMIT_CPU, &LIM);
@@ -108,75 +162,188 @@ public:
             setrlimit(RLIMIT_AS, &LIM);//设置进程的虚拟地址空间大小，这里是 500mb
 
             stderr=freopen("ce.txt", "w", stderr);
-            //freopen("/dev/null", "w", stdout);
+            freopen("/dev/null", "w", stdout);
+            execvp("/bin/gcc",(char *const *)"main.c -o main");
 
-           // execute_cmd("/bin/chown judge %s ", box.workdir.c_str());//将工作目录的所有者设置为judge
-          //  execute_cmd("/bin/chmod 750 %s ", box.workdir.c_str());//并赋予相应的权限
+            std::cout<<"编译完成！"<<std::endl;
 
-
-            /*
-            X_OK 值为1，判断对文件是可执行权限
-
-            W_OK 值为2，判断对文件是否有写权限
-
-            R_OK 值为4，判断对文件是否有读权限
-
-            注：后三种可以使用或“|”的方式，一起使用，如W_OK|R_OK
-            */
-            if (access("usr", F_OK ) == -1){//这里就是判断usr目录是否存在，失败返回 -1 ，正常情况下，上面已经清理过了工作目录usr肯定不存在
-                //准备编译环境
-
-            }
-           // if(chroot(work_dir.c_str())) printf("warning chroot fail!\n");
-
-            while (setgid(1536) != 0)
-                sleep(1);
-            while (setuid(1536) != 0)
-                sleep(1);
-            while (setresuid(1536, 1536, 1536) != 0)
-                sleep(1);
-
-            /*****************************************各个语言的编译环境已经搭建好了，下面是执行编译命令*********************/
-//            switch (lang)
-//            {
-//                case 0:
-//                    execvp(CP_C[0], (char *const *)CP_C);
-//                    break;
-//                case 1:
-//                    execvp(CP_X[0], (char *const *)CP_X);
-//                    break;
-//
-//                default:
-//                    printf("nothing to do!\n");
-//            }
-//
-//
-//            cout<<"编译完成！"<<endl;
-            //exit(!system("cat ce.txt"));
             exit(0);
         }
         else//父进程
         {
             int status = 0;
-
             waitpid(pid, &status, 0);//阻塞的等待编译命令执行完
-
-            //execute_cmd("/bin/umount -l bin usr lib lib64 etc/alternatives dev 2>/dev/null");//卸载目录
-            //execute_cmd("/bin/umount -r %s/* 2>/dev/null", work_dir);
-            //umount(work_dir);//清理一遍
-
             return status;
         }
     }
     //运行
+    int get_proc_status(int pid, const char *mark)
+    {
+        FILE *pf;
+        char fn[BUFFER_SIZE], buf[BUFFER_SIZE];
+        int ret = 0;
+        sprintf(fn, "/proc/%d/status", pid);
+        pf = fopen(fn, "re");
+        int m = strlen(mark);
+        while (pf && fgets(buf, BUFFER_SIZE - 1, pf))//循环查找/proc/%d/status文件里对应参数的值
+        {
+
+            buf[strlen(buf) - 1] = 0;
+            if (strncmp(buf, mark, m) == 0)//找到对应的参数
+            {
+                if(1!=sscanf(buf + m + 1, "%d", &ret)) printf("proc read fail\n");//将参数值写入到ret里
+            }
+        }
+        if (pf)
+            fclose(pf);
+        return ret;
+    }
+    long get_file_size(const char *filename)
+    {
+        struct stat f_stat;
+
+        if (stat(filename, &f_stat) == -1)
+        {
+            return 0;
+        }
+
+        return (long)f_stat.st_size;
+    }
+    void tracer(const int &pid,const int &time_lmt,const int &mem_lmt,int &curmemory,int &usedtime){
+
+        int status, sig, exitcode;
+        struct rusage ruse;
+        int first = true;
+        while (true)
+        {
+            wait4(pid, &status, __WALL, &ruse);     //等待子进程切换内核态（调用系统API或者运行状态变化） ，其中ruse是为了获取cpu使用时间
+            if (first)
+            { //
+                ptrace(PTRACE_SETOPTIONS, pid, NULL, PTRACE_O_TRACESYSGOOD | PTRACE_O_TRACEEXIT);
+            }
+            curmemory = get_proc_status(pid, "VmPeak:") << 10;
+            if (curmemory > mem_lmt * STD_MB)
+            {
+                report.upStatus(23);//内存超出限制
+                ptrace(PTRACE_KILL, pid, NULL, NULL);
+                break;
+            }
+
+            ////1 compiling ,2 running 11 compile error  21 run error 22 TL  23 ML  24 OL 3 ok 31 answer error
+            if (WIFEXITED(status)) {
+                std::cout<<"正常结束,状态码号："<<WEXITSTATUS(status)<<" "<<strsignal(WEXITSTATUS(status))<<std::endl;
+                break;
+            }
+            if (WIFSIGNALED(status))
+            {
+                /*  WIFSIGNALED: if the process is terminated by signal
+                 *  由外部信号触发的进程终止
+                 *  psignal(int sig, char *s)，like perror(char *s)，print out s, with error msg from system of sig
+                 * sig = 5 means Trace/breakpoint trap
+                 * sig = 11 means Segmentation fault
+                 * sig = 25 means File size limit exceeded
+                 */
+                sig = WTERMSIG(status);
+
+                switch (exitcode)                  // 根据退出的原因给出判题结果
+                {
+                    case SIGCHLD:
+                    case SIGALRM:
+                        alarm(0);
+                        printf("alarm:%g\n", time_lmt);
+                    case SIGKILL:
+                    case SIGXCPU:
+                        report.upStatus(22);//超时
+                        //usedtime = time_lmt * 1000;
+                        break;
+                    case SIGXFSZ:
+                        report.upStatus(24);//输出超出限制
+                        break;
+                    default:
+                        report.upStatus(23);//运行出错;
+                }
+                //print_runtimeerror(infile+strlen(oj_home)+5,strsignal(exitcode));
+                std::cout<<strsignal(exitcode)<<std::endl;
+                break;
+            }
+
+
+            long  call_id = ptrace(PTRACE_PEEKUSER, pid, ORIG_RAX * 8, NULL);
+            std::cout<<"系统回调id："<<call_id<<std::endl;
+            // 白名单机制
+            ptrace(PTRACE_SYSCALL, pid, NULL, NULL);    // 继续等待下一次的系统调用或其他中断
+
+        }
+        usedtime += (ruse.ru_utime.tv_sec * 1000 + ruse.ru_utime.tv_usec / 1000) ; // 统计用户态耗时，在更快速的CPU上加以cpu_compensation倍数放大
+        usedtime += (ruse.ru_stime.tv_sec * 1000 + ruse.ru_stime.tv_usec / 1000) ; // 统计内核态耗时，在更快速的CPU上加以cpu_compensation倍数放大
+    }
+    void tracee(const int &time_lmt,const int &mem_lmt){
+        char * const envp[]={(char * const )"PYTHONIOENCODING=utf-8",
+                             (char * const )"LANG=zh_CN.UTF-8",
+                             (char * const )"LANGUAGE=zh_CN.UTF-8",
+                             (char * const )"LC_ALL=zh_CN.utf-8",NULL};
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        if(nice(19)!=19) printf("......................renice fail... \n");
+
+        // trace me
+
+
+        // now the user is "judger"
+        if(box.into()==0){
+            std::cout<<"正常进入沙盒\n";
+        }
+
+
+        struct rlimit LIM; // time limit, file limit& memory limit
+        LIM.rlim_cur = time_lmt;
+        LIM.rlim_max = time_lmt;
+        setrlimit(RLIMIT_CPU, &LIM);
+        alarm(0);
+        alarm(time_lmt);
+
+        // file limit
+        LIM.rlim_max = STD_MB<<5 + STD_MB;
+        LIM.rlim_cur = STD_MB<<5;
+        setrlimit(RLIMIT_FSIZE, &LIM);
+
+        // proc limit
+        LIM.rlim_cur = LIM.rlim_max = 1;
+        setrlimit(RLIMIT_NPROC, &LIM);
+
+        // set the stack
+        LIM.rlim_cur = STD_MB << 8;
+        LIM.rlim_max = STD_MB << 8;
+        setrlimit(RLIMIT_STACK, &LIM);
+
+        // set the memory
+        LIM.rlim_cur = STD_MB * mem_lmt / 2 * 3;
+        LIM.rlim_max = STD_MB * mem_lmt * 2;
+        setrlimit(RLIMIT_AS, &LIM);//虚拟内存限制
+
+
+        execle("./main", "./main", (char *)NULL,envp);
+    }
     bool run(){
+
+        int mem_lmt=64;
+        int  time_lmt=1;
+        int usedtime=0;
+        int curmemory = 0;
+        pid_t pid = fork();
+        if (pid == 0){
+            tracee(time_lmt,mem_lmt);
+        }
+        else{
+            tracer(pid,time_lmt,mem_lmt,curmemory,usedtime);
+        }
+
 
     }
     //对比输出文件
     bool diff(){
 
     }
-    ~tracer(){
+    ~Judger(){
 
     }
 
@@ -199,7 +366,7 @@ int main(int argc,char *argv[]){
     int langid=atoi(argv[4]);
     language_set langs("langs.conf");
     if(langs.exist(langid)){
-        tracer tc(argv[1],port,langs.get(langid),"workdir");
+        Judger tc(argv[1],port,langs.get(langid),"workdir");
         if(tc.compile()){
             if(tc.run()){
                 tc.diff();
